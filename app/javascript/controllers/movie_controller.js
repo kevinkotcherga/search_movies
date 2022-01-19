47e3284d7d1895b92127b07698cd4f346a10821d
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-19 17:23:08 +0100

    ✨feat(javascript): ajout de l'api omdb et de la fonction recherche ajax

---
+++ app/javascript/controllers/movie_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    const searchMovies = (query) => {
+      fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+        .then(response => response.json())
+        .then((data) => {
+          data.Search.forEach((result) => {
+            const movie = `<li class="list-inline-item">
+              <img src="${result.Poster}" alt="">
+              <p>${result.Title}</p>
+            </li>`;
+            results.insertAdjacentHTML("beforeend", movie);
+          });
+        });
+    };
+    const form = document.querySelector('#search-form');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // <-- to prevent <form>'s native behaviour
+      const input = event.currentTarget.querySelector('.form-control');
+      results.innerHTML = '';
+      searchMovies(input.value);
+    });
+  }
+}
